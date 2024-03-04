a3f0ddad01b9baa36954676c4d6a844c418f8b3f
Tony081113 <141484094+Tony081113@users.noreply.github.com>
2024-03-04 11:09:46 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
-basic.showNumber(0)
+input.onButtonPressed(Button.B, function () {
+    if (數字 == 2) {
+        數字 = 1
+    } else {
+        數字 += 1
+    }
+})
+let 顯示 = 0
+let 數字 = 0
+數字 = 1
+while (顯示 == 0) {
+    basic.showNumber(數字)
+}
 basic.forever(function () {
 	
 })
